377d3c8e40eff87e7642023e03ae5aacaf0b1d3a
TongZhang <temberature@gmail.com>
2022-09-03 15:07:25 +0800

    run only once.

--- examples.py
+++ examples.py
@@ -9,36 +9,36 @@ default_smtp_server = 'smtp.example.com'
 default_email_sender = 'hypothesis.notifier@example.com'
 default_email_password = 'q1...Nm'
 
-while True:
-    try:
-        # slack recipes
+# while True:
+try:
+    # slack recipes
 
-        # notify when an url in a list of urls of interest to the team is annotated
-        # uses the wildcard_api endpoint
-        notify_slack_url_activity(url='https://web.hypothes.is/*', token=default_token, pickle='urls', channel='test', hook=default_hook)
+    # notify when an url in a list of urls of interest to the team is annotated
+    # uses the wildcard_api endpoint
+    notify_slack_url_activity(url='https://web.hypothes.is/*', token=default_token, pickle='urls', channel='test', hook=default_hook)
 
-        # notify when there's an annotation from one among a list of users
-        users = ['remiholden', 'jeremydean']
-        for user in users:
-            notify_slack_user_activity(user=user, token=default_token, pickle='users', channel='test', hook=default_hook) 
+    # notify when there's an annotation from one among a list of users
+    users = ['remiholden', 'jeremydean']
+    for user in users:
+        notify_slack_user_activity(user=user, token=default_token, pickle='users', channel='test', hook=default_hook) 
 
-        # notify when there's an annotation in a group
-        notify_slack_group_activity(group='8gk9i7VV', groupname='Anyone Can Join', token=default_token, pickle='AnyoneCanJoin', channel='test', hook=default_hook)
+    # notify when there's an annotation in a group
+    notify_slack_group_activity(group='8gk9i7VV', groupname='Anyone Can Join', token=default_token, pickle='AnyoneCanJoin', channel='test', hook=default_hook)
 
-        # notify when there's an annotation with a specified tag
-        notify_slack_tag_activity(tag='nextprez', token=default_token, pickle='nextprez', channel='test', hook=default_hook)
+    # notify when there's an annotation with a specified tag
+    notify_slack_tag_activity(tag='nextprez', token=default_token, pickle='nextprez', channel='test', hook=default_hook)
 
-        # rss recipes 
+    # rss recipes 
 
-        notify_rss_group_activity(group='8gk9i7VV', groupname='Anyone Can Join', token=default_token, pickle='8gk9i7VV')
+    notify_rss_group_activity(group='8gk9i7VV', groupname='Anyone Can Join', token=default_token, pickle='8gk9i7VV')
 
-        # email recipes
+    # email recipes
 
-        notify_email_tag_activity(tag='futuoer', token=default_token, pickle='futuoer', smtp=default_smtp_server, sender=default_email_sender, sender_password=default_email_password, recipient='nobody@example.com')
+    notify_email_tag_activity(tag='futuoer', token=default_token, pickle='futuoer', smtp=default_smtp_server, sender=default_email_sender, sender_password=default_email_password, recipient='nobody@example.com')
 
-        print ( 'sleeping' )
-        time.sleep(60 * 30)  # wait 30 min to be polite to the hypothesis api
-    except:
-        print ( traceback.print_exc() )
+    # print ( 'sleeping' )
+    # time.sleep(60 * 30)  # wait 30 min to be polite to the hypothesis api
+except:
+    print ( traceback.print_exc() )
 
 
